5d1545e4020f89fbd9547e3e2cadac027f1cbddd
Bobby Melhem <bobbyjacksonmelhem@gmail.com>
2021-08-21 14:06:07 +1000

    user.js added

---
+++ Server/models/user.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize')
+  
+const sequelize = require('../utils/database')
+  
+// Define User table
+const User = sequelize.define('user', {
+    sid:{
+        type:Sequelize.STRING(9),
+        allowNull:false,
+        primaryKey:true
+    },
+
+    firstName: { type: Sequelize.STRING, allowNull:false },
+
+    lastName: { type: Sequelize.STRING, allowNull:false },
+
+    email: { type: Sequelize.STRING, allowNull:false },
+
+    phone: { type: Sequelize.INTEGER(10) },
+
+    bio: { type: Sequelize.TEXT },
+
+    picure: { type: Sequelize.STRING },
+
+    tokens: { type: Sequelize.INTEGER }
+})
+  
+module.exports = User
