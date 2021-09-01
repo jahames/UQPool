46477757a5b39aaa7cf29835f0e5c6630fd48c8e
Yujun Li <yujunli@Yujuns-iMac.local>
2021-09-01 20:21:42 +1000

    wtf is going on

---
+++ clientside/screens/HistoryScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Pressable } from 'react-native';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+function HistoryScreen(props) {
+    return (
+        <SafeAreaView>
+
+            <Text>back to user screen</Text>
+
+        </SafeAreaView>
+    );
+}
+
+export default HistoryScreen;
